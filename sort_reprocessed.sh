e6dc53aff008b5ffb41c6d7afd7db33455de1381
rodrigo <rodrigo@gato>
2016-12-24 17:41:14 -0300

    Updated dependencies

--- sort_reprocessed.sh
+++ sort_reprocessed.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 folder=./output/
-set1=./set1/
-set2=./set2/
+set1=./beer_drill/
+set2=./hammer/
+set3=./cube_block/
+
+
 
 cd $folder
 
 rm -rf $set1
 mkdir -p $set1
+
 rm -rf $set2
 mkdir -p $set2
 
+rm -rf $set3
+mkdir -p $set3
+
+
+
 mv ./cordless_drill* $set1
 mv ./thin_beer* $set1
-mv ./wood_cube* $set1
 
-mv ./hammer* $set2
-mv ./cinder_block* $set2
+mv ./hammer*.yaml $set2
+
+mv ./wood_cube* $set3
+mv ./cinder_block* $set3
