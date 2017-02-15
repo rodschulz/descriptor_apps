6c5fcc65d44210fb7ca979662cc5934f10686b8d
rodrigo <rodrigo@gato>
2017-02-15 15:07:27 -0300

    Added logging for unit tests

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,6 +1,28 @@
 /**
  * Author: rodrigo
- * 2016     
+ * 2016
  */
 #define BOOST_TEST_MODULE TestMain
+
 #include <boost/test/included/unit_test.hpp>
+#include <yaml-cpp/yaml.h>
+#include <plog/Log.h>
+#include <plog/Appenders/ColorConsoleAppender.h>
+
+
+#define LOGGING_LOCATION "config/logging.yaml"
+
+
+struct LogggingSetup
+{
+	LogggingSetup()
+	{
+		static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+		plog::init(plog::severityFromString(YAML::LoadFile(LOGGING_LOCATION)["level"].as<std::string>().c_str()), &consoleAppender);
+	}
+	~LogggingSetup()  {
+	}
+};
+
+
+BOOST_GLOBAL_FIXTURE(LogggingSetup);
